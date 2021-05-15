3042cc7974664a19227da30ae16110d31830164a
paulsok <paul.s.falke@gmail.com>
2021-05-15 15:49:06 +0200

    median of two sorted arrays fix

--- leetcode/median_two_lists.py
+++ leetcode/median_two_lists.py
@@ -1,3 +1,11 @@
+# Input: nums1 = [1,2], nums2 = [3,4]
+# Output: 2.50000
+# Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+# Input: nums1 = [1,3], nums2 = [2]
+# Output: 2.00000
+# Explanation: merged array = [1,2,3] and median is 2.
+
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2) -> float:
         new_list = sorted(nums1 + nums2)
