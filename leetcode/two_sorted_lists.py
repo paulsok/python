6c7db8a65d7863d65e7a95c9752ef98857674379
paulsok <paul.s.falke@gmail.com>
2021-05-20 23:42:41 +0200

    sort two linked lists

---
+++ leetcode/two_sorted_lists.py
@@ -0,0 +1,27 @@
+# Example 1:
+# Input: l1 = [1,2,4], l2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+
+# Example 2:
+# Input: l1 = [], l2 = []
+# Output: []
+
+# Example 3:
+# Input: l1 = [], l2 = [0]
+# Output: [0]
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            l1.next = self.mergeTwoLists(l1.next, l2)
+
+        return l1 or l2
