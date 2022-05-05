7dd5f1a2b9c52c0e8d367a5469ed7ae448f0edcc
paulsok <paul.s.falke@gmail.com>
2022-05-05 22:59:03 +0300

    two pointers

---
+++ practicum/two_pointers.py
@@ -0,0 +1,14 @@
+def get_next_dif_letter_idx(input_str, cur_letter_idx):
+    next_letter_idx = cur_letter_idx
+    while next_letter_idx < len(input_str) \
+    and input_str[next_letter_idx] == input_str[cur_letter_idx]:
+        next_letter_idx += 1
+    return next_letter_idx
+
+def max_consecutive_elements(input_str):
+    result, cur_letter_idx = 0, 0
+    while cur_letter_idx < len(input_str):
+        next_letter_idx = get_next_dif_letter_idx(input_str, cur_letter_idx)
+        result = max(result, next_letter_idx - cur_letter_idx)
+        cur_letter_idx += 1
+    return result 
