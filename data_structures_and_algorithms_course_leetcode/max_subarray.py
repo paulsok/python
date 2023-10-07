39da374fd5d50bd1ff8b9caa1ed64416f5091dec
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2023-10-07 16:16:00 +0200

    week1: add max. subarray

---
+++ data_structures_and_algorithms_course_leetcode/max_subarray.py
@@ -0,0 +1,11 @@
+def find_length(nums, k):
+    # curr is the current sum of the window
+    left = curr = ans = 0
+    for right in range(len(nums)):
+        curr += nums[right]
+        while curr > k:
+            curr -= nums[left]
+            left += 1
+        ans = max(ans, right - left + 1)
+
+    return ans
