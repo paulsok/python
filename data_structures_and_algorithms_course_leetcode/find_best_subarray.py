41de360c6dc03ea4cc1454a44f3ea95b6415d609
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2023-10-07 16:49:03 +0200

    week1: add find best subarray

---
+++ data_structures_and_algorithms_course_leetcode/find_best_subarray.py
@@ -0,0 +1,11 @@
+def find_best_subarray(nums, k):
+    curr = 0
+    for i in range(k):
+        curr += nums[i]
+
+    ans = curr
+    for i in range(k, len(nums)):
+        curr += nums[i] - nums[i - k]
+        ans = max(ans, curr)
+
+    return ans
