18c0138a4f324395b8a6aad3e25079b37b5b40ce
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2023-10-13 18:57:40 +0200

    week1: add running sum

---
+++ data_structures_and_algorithms_course_leetcode/running_sum.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        prefix = [nums[0]]
+        for i in range(1, len(nums)):
+            prefix.append(nums[i] + prefix[-1])
+        return prefix
