9dd02c2730075760f33af200458f05540e25bf7b
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2023-10-21 17:50:54 +0200

    week2: add two sum

---
+++ data structures and algorithms course leetcode/hashing/two_sum.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+            num = nums[i]
+            complement = target - num
+            if complement in dic:  # This operation is O(1)!
+                return [i, dic[complement]]
+
+            dic[num] = i
+
+        return [-1, -1]
