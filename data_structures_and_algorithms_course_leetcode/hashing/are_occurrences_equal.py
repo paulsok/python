be48b9edad4626f2b29620632a7c77029d265a8b
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2023-10-29 22:25:17 +0100

    week2: add subarray sum

---
+++ data_structures_and_algorithms_course_leetcode/hashing/are_occurrences_equal.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        counts = defaultdict(int)
+        for c in s:
+            counts[c] += 1
+
+        frequencies = counts.values()
+        return len(set(frequencies)) == 1
+
+
+# class Solution:
+#     def areOccurrencesEqual(self, s: str) -> bool:
+#         return len(set(Counter(s).values())) == 1
