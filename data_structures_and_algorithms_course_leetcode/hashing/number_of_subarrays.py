74f524dcbfeb0e7dc19cdb035b3fec12307d83ff
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2023-10-29 22:25:40 +0100

    week2: add number of subarrays

---
+++ data_structures_and_algorithms_course_leetcode/hashing/number_of_subarrays.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        counts = defaultdict(int)
+        counts[0] = 1
+        ans = curr = 0
+
+        for num in nums:
+            curr += num % 2
+            ans += counts[curr - k]
+            counts[curr] += 1
+
+        return ans
