bd6aa3ed0e9fba36bd861cb464dffa09411be37a
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2023-10-30 00:32:36 +0100

    week2: add largest unique number

---
+++ data_structures_and_algorithms_course_leetcode/hashing/largest_unique_number.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def largestUniqueNumber(self, nums: List[int]) -> int:
+        default_dict = defaultdict(int)
+
+        for num in nums:
+            default_dict[num] += 1
+
+        ans = -1
+        for key in default_dict.keys():
+            if default_dict[key] == 1 and key > ans:
+                ans = key
+        return ans
