b0e34056aa0b91bc56c8e3dbb511d04272f58649
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2023-11-01 17:17:07 +0100

    week2: add maximum sum

---
+++ data_structures_and_algorithms_course_leetcode/hashing/maximum_sum.py
@@ -0,0 +1,23 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def maximumSum(self, nums: List[int]) -> int:
+        def get_digit_sum(num):
+            digit_sum = 0
+            while num:
+                digit_sum += num % 10
+                num //= 10
+
+            return digit_sum
+
+        dic = defaultdict(int)
+        ans = -1
+        for num in nums:
+            digit_sum = get_digit_sum(num)
+            if digit_sum in dic:
+                ans = max(ans, num + dic[digit_sum])
+            dic[digit_sum] = max(dic[digit_sum], num)
+
+        return ans
