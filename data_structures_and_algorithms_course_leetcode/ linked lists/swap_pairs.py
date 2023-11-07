76910a520c8f17a6592c493f72d143596b5f4b74
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2023-11-07 20:32:37 +0100

    week3: add swap pairs

---
+++ data_structures_and_algorithms_course_leetcode/ linked lists/swap_pairs.py
@@ -0,0 +1,23 @@
+from leetcode.linked_list import ListNode
+
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        # Check edge case: linked list has 0 or 1 nodes, just return
+        if not head or not head.next:
+            return head
+
+        dummy = head.next               # Step 5
+        prev = None                     # Initialize for step 3
+        while head and head.next:
+            if prev:
+                prev.next = head.next   # Step 4
+            prev = head                 # Step 3
+
+            next_node = head.next.next  # Step 2
+            head.next.next = head       # Step 1
+
+            head.next = next_node       # Step 6
+            head = next_node            # Move to next pair (Step 3)
+
+        return dummy
