a3afb397a3e7bf80a8d1ad38c0e438d80b42c7c0
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2023-12-20 19:50:56 +0100

    week5: add lowest common ancestor

---
+++ data_structures_and_algorithms_course_leetcode/trees and graphs/lowest_common_ancestor.py
@@ -0,0 +1,24 @@
+from practicum.tree_node import TreeNode
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+
+        # first case
+        if root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        # second case
+        if left and right:
+            return root
+
+        # third case
+        if left:
+            return left
+
+        return right
