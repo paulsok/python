1a35f1a18dac3863aebf2953471585db7c915f8e
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2023-12-29 13:10:29 +0100

    week5: add insert binary search tree

---
+++ data_structures_and_algorithms_course_leetcode/trees and graphs/insert_binary_search_tree.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+from practicum.tree_node import TreeNode
+
+
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        
+        return root
