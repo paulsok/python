41f3b364ed5e357be485a9c26676f8afc4a8b464
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2023-12-29 11:20:33 +0100

    week5: add range sum binary search tree

---
+++ data_structures_and_algorithms_course_leetcode/trees and graphs/range_sum_binary_search_tree.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from practicum.tree_node import TreeNode
+
+
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        if not root:
+            return 0
+
+        ans = 0
+        if low <= root.val <= high:
+            ans += root.val
+        if low < root.val:
+            ans += self.rangeSumBST(root.left, low, high)
+        if root.val < high:
+            ans += self.rangeSumBST(root.right, low, high)
+
+        return ans
