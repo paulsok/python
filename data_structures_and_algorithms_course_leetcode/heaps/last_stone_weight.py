053ecbfac7d117099f16b513314189f3c75371a4
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2024-03-19 20:17:55 +0100

    week6: add last stone weight

---
+++ data_structures_and_algorithms_course_leetcode/heaps/last_stone_weight.py
@@ -0,0 +1,15 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-stone for stone in stones]
+        heapq.heapify(stones) # turns an array into a heap in linear time
+        while len(stones) > 1:
+            first = abs(heapq.heappop(stones))
+            second = abs(heapq.heappop(stones))
+            if first != second:
+                heapq.heappush(stones, -abs(first - second))
+
+        return -stones[0] if stones else 0
