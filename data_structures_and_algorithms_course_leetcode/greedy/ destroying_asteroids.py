b17c9f88b26b023b5938a6f1e0764537f3fd3cfa
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2024-04-06 22:17:50 +0100

    week7: add destroying asteroids

---
+++ data_structures_and_algorithms_course_leetcode/greedy/ destroying_asteroids.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+        for asteroid in asteroids:
+            if asteroid > mass:
+                return False
+            mass += asteroid
+        
+        return True
