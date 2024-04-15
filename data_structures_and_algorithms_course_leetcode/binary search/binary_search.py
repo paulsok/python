3c085c3abb4182bbdf93cff5d72fe17ab1ae09b8
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2024-04-15 18:08:10 +0100

    week8: add binary search

---
+++ data_structures_and_algorithms_course_leetcode/binary search/binary_search.py
@@ -0,0 +1,15 @@
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            # do something
+            return
+        if arr[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    # target is not in arr, but left is at the insertion point
+    return left
