178e8784dde09237887a03806ad0974d31798013
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2024-04-15 18:47:22 +0100

    week8: add search insert position

---
+++ data_structures_and_algorithms_course_leetcode/binary search/search_insert_position.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            num = nums[mid]
+
+            if num == target:
+                return mid
+
+            if num > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return right
