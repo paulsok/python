2bb361b03494cc8fba499d87b4e18a9b804a95e2
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2024-05-14 18:04:36 +0100

    week10: add min cost climbing stairs - bottom-up

--- data_structures_and_algorithms_course_leetcode/dynamic programming/min_cost_climbing_stairs.py
+++ data_structures_and_algorithms_course_leetcode/dynamic programming/min_cost_climbing_stairs.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# Top-down approach
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         # 1. A function that returns the answer
@@ -18,3 +19,21 @@ class Solution:
         
         memo = {}
         return dp(len(cost))
+
+
+# Bottom-up approach
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        # Step 2
+        dp = [0] * (n + 1)
+        
+        # Step 3: Base cases are implicitly defined as they are 0
+
+        # Step 4
+        for i in range(2, n + 1):
+            # Step 5
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+        
+        # Step 6
+        return dp[n]
