0191a5263ff14fbb6e75831a9bcc5361d5a0f0df
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2024-05-26 20:04:09 +0100

    week10: add unique paths - bottom-up

--- data_structures_and_algorithms_course_leetcode/dynamic programming/unique_paths.py
+++ data_structures_and_algorithms_course_leetcode/dynamic programming/unique_paths.py
@@ -1,6 +1,6 @@
 from functools import cache
 
-
+# Top-down approach
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         @cache
@@ -16,3 +16,19 @@ class Solution:
             return ways
         
         return dp(m - 1, n - 1)
+    
+
+# Bottom-up approach
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1
+        
+        for row in range(m):
+            for col in range(n):
+                if row > 0:
+                    dp[row][col] += dp[row - 1][col]
+                if col > 0:
+                    dp[row][col] += dp[row][col - 1]
+
+        return dp[m - 1][n - 1]
