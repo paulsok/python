0ff1d61dd3d5bcae9eab9b440cd0fd4769d7b8fe
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2024-05-30 22:19:27 +0100

    week11: add better car pooling

--- data_structures_and_algorithms_course_leetcode/bonus/car_pooling.py
+++ data_structures_and_algorithms_course_leetcode/bonus/car_pooling.py
@@ -1,17 +1,18 @@
 from typing import List
 
 
-class Solution:
-    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        arr = [0] * (max(trip[2] for trip in trips) + 1)
-        for (value, left, right) in trips:
-            arr[left] += value
-            arr[right] -= value
+def find_brightest_position(lights: List[List[int]]) -> int:
+    change = []
+    for position, radius in lights:
+        change.append([position - radius, 1])
+        change.append([position + radius + 1, -1])
 
-        curr = 0
-        for i in range(len(arr)):
-            curr += arr[i]
-            if curr > capacity:
-                return False
-        
-        return True
+    change.sort()
+    ans = curr = brightest = 0
+    for position, value in change:
+        curr += value
+        if curr > brightest:
+            brightest = curr
+            ans = position
+
+    return ans
