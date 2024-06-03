a2289855d6963fc74d3bb3f38c2ceda16a00ac79
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2024-06-03 13:50:01 +0100

    week11: add single number

---
+++ data_structures_and_algorithms_course_leetcode/bonus/single_number.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        mask = 0
+        for num in nums:
+            mask ^= num
+        
+        return mask
