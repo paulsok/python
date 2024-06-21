19c5c71244a1fa8b7827bbfb2eaccebb88680ae2
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2024-06-21 20:15:56 +0100

    week11: add maximum product after k increments

---
+++ data_structures_and_algorithms_course_leetcode/bonus/maximum_product_after_k_increments.py
@@ -0,0 +1,17 @@
+from typing import List
+import heapq
+
+
+class Solution:
+    def maximumProduct(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+
+        for _ in range(k):
+            heapq.heappush(nums, heapq.heappop(nums) + 1)
+
+        MOD = 1_000_000_007
+        ans = 1
+        for x in nums:
+            ans *= x
+
+        return ans % MOD
